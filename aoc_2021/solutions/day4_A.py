e308998e7a3ac200f9c7a7ce68d43534851545fe
Frida Gidlund <frida_gidde95@hotmail.com>
2021-12-05 06:47:21 +0100

    day4 start

---
+++ aoc_2021/solutions/day4_A.py
@@ -0,0 +1,14 @@
+import helpers as helper
+
+input_path = "inputs\\day4_1_numbers.txt"
+file_path = helper.get_relative_dir(input_path)
+input_numbers = helper.get_file_as_list(file_path)
+
+input_path = "inputs\\day4_1_bingo.txt"
+file_path = helper.get_relative_dir(input_path)
+input_bingo = helper.get_file_as_list(file_path)
+
+
+
+
+
